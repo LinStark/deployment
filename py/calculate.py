7e5da3057baf2f17a68a83b2d39732d670dec1fa
Cloud User <centos@manabo-4.novalocal>
2020-09-15 08:35:38 +0000

    添加跨片比率test

--- py/calculate.py
+++ py/calculate.py
@@ -1,20 +1,12 @@
 import sys
-data = []
-sum = 0
-count = 0
-shard_count = int(sys.argv[1])
-n = int(sys.argv[2])
-for line in open("sum.log","r"):
-	a = line[:-1]
-	a = float(a)
-	sum = sum+a
-	count = count+1
-if shard_count == 0:
-	print (sum)
-else:
-	if n > 10000:
-		print (sum/count)
-	else:
-		print( sum/((1/n)+1))
 
+sum = 0
+rate = 0.0
+for line in open("sum.log","r"):
+	a = line
+	data=a.split(" ")
+	tps = data[0]
+	rate= data[1]
+	sum = sum+float(tps)
+print (rate,sum)
 
--- py/test.py
+++ py/test.py
@@ -7,33 +7,22 @@ r = sys.argv[2]
 T = sys.argv[3]
 rate = sys.argv[5]  # rate
 shard="0"
-if shard_count == 1:
-
-    cmd = "cd dist;./tm-bench -rate " + rate + " -r " + r + " -T " + T +" -shard "+shard+ " 0S1.test:26657"
+if shard_count == 1: # 单分片的情况下
+    cmd = "cd dist;./tm-bench -rate " + rate + " -r " + r + " -T " + T +" -as 0 0S1.test:26657"
     print(cmd)
 else:
     Send_shard = ""  # 要发往哪些分片
+    shard_node = ""
     for i in range(0, shard_count ):
         a = str(i)
         if i == (shard_count-1):
             Send_shard = Send_shard + a
+            shard_node = shard_node + a+"S1.test:26657"
         else:
             Send_shard = Send_shard + a + ","
+            shard_node = shard_node + a+"S1.test:26657,"
     if shard_count == 0:
         Send_shard = Send_shard + ",1"
-    flag = 0
-    for i in range(0, shard_count ):
-        shard_name = str(i)
-        shard_node = shard_name + "S1.test:26657"
-        cmd = "cd dist;./tm-bench -rate " + rate + " -r " + r + " -T " + T + " -shard " +shard_name+" -as " + Send_shard + " " + shard_node
-        if i == (shard_count-1):
-            cmd = "./tm-bench -rate " + rate + " -r " + r + " -T " + T + " -shard " +shard_name+" -as " + Send_shard + " " + shard_node
-            cmd_set = cmd_set + " &" + cmd
-        else:
-            if flag == 0:
-                cmd_set = cmd
-                flag = 1
-            else:
-                cmd1 = "./tm-bench -rate " + rate + " -r " + r + " -T " + T + " -shard " + shard_name + " -as " + Send_shard + " " + shard_node
-                cmd_set = cmd_set + " &" + cmd1
-    print(cmd_set)
+    cmd = "cd dist;./tm-bench -rate " + rate + " -r " + r + " -T " + T +" -as " + Send_shard + " -datafile token_transfers.csv " + shard_node
+
+    print(cmd)
